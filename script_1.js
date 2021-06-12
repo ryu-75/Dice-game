2835e23d4583462b8c5164f8f3e616a6cca3d568
Nicolas L <lorion06@gmail.com>
2021-06-12 13:46:21 +0200

    feat : add a functionnality to hold btn

--- script_1.js
+++ script_1.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
     let activePlayer = 0;
     let playing = true;
 
-    $('.dice').hide();
-   
+    $('.dice').hide()
+    
+    //Rolling dice functionnality
     $('.btn--roll').click(() => {
         if (playing) {
             const dice = Math.trunc(Math.random() * 6) + 1;
@@ -18,14 +19,54 @@ $(document).ready(function() {
             if (dice !== 1) {
                 currentScore += dice;
                 activePlayer = activePlayer === 0 ? 1 : 0;
-                $('current', activePlayer) 
+                $('.current', activePlayer) 
                 textContent = currentScore; 
             } else {
-                activePlayer = active === 0 ? 1 : 0;
-                $('current', activePlayer)
+                activePlayer = activePlayer === 0 ? 1 : 0;
+                $('.current', activePlayer)
                 textContent = currentScore;
             }
         }
+    });
+
+    //hold value to current score 
+    $('.btn--hold').click(() => {
+        if (playing) {
+            let score = [0, 0]      
+            score[activePlayer] += currentScore
+
+            $(`#score-${activePlayer}`).text(score[activePlayer])
+
+            if (score[activePlayer] >= 100) {
+                playing = false;
+                $('.dice').hide()
+
+                $(`.player-${activePlayer}`).attr('winner-player')
+                $(`.player-${activePlayer}`).remove('player-active')
+            } else {
+                activePlayer = activePlayer === 0 ? 1 : 0;
+                $('.current', activePlayer)
+                textContent = currentScore;
+            }
+        }
+    });
+
+    $('.btn--new').click(() => {
+        scores = [0, 0];
+        currentScore = 0;
+        activePlayer = 0;
+        playing = true;
+
+        $('#score-0').text('0')
+        $('#score-1').text('0')
+        $('#current-0').text('0')
+        $('#current-1').text('0')
+
+        $('.dice').hide()
+        $('.player-0').remove('player-winner')
+        $('.player-1').remove('player-winner')
+        $('.player-0').add('player-active')
+        $('.player-1').remove('player-active')
     })
 
 })
