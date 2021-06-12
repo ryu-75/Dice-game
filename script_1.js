45c61f2a5294332674ae51f2b947615fbc64167a
Nicolas L <lorion06@gmail.com>
2021-06-12 13:08:24 +0200

    feat: parameter roll dice, show after to click btn--roll and change png

--- script_1.js
+++ script_1.js
@@ -1,3 +1,31 @@
-'use strict'
+$(document).ready(function() {
 
-$('')
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
+
+    $('.dice').hide();
+   
+    $('.btn--roll').click(() => {
+        if (playing) {
+            const dice = Math.trunc(Math.random() * 6) + 1;
+
+            $('.btn--roll').click(() => {
+                $('.dice').show()
+                $('.dice').attr('src', `./dice-element/dice-${dice}.png`)
+            })
+
+            if (dice !== 1) {
+                currentScore += dice;
+                activePlayer = activePlayer === 0 ? 1 : 0;
+                $('current', activePlayer) 
+                textContent = currentScore; 
+            } else {
+                activePlayer = active === 0 ? 1 : 0;
+                $('current', activePlayer)
+                textContent = currentScore;
+            }
+        }
+    })
+
+})
