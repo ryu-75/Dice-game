81733c5ade413e8d921bb2957a343143072441b5
Nicolas L <lorion06@gmail.com>
2021-06-12 20:44:21 +0200

    feat : display score value into current

--- script_1.js
+++ script_1.js
@@ -1,77 +1,86 @@
 $(document).ready(function() {
 
-    let currentScore = 0;
-    let activePlayer = 0;
-    let playing = true;
+    let player0El = $('.player-0')
+    let player1El = $('.player-1')
+    let score0El = $('#score-0');
+    let score1El = $('#score-1');
+    let current0El = $('#current-0');
+    let current1El = $('#current-1');
 
-    $('.dice').hide();
+    let diceEl = $('.dice');
+    let btnNew = $('.btn--new');
+    let btnRoll = $('.btn--roll');
+    let btnHold = $('.btn--hold');
     
+
+    let scores, activePlayer, currentScore; 
+    
+    //Starting condition
     const init = (() => {
         scores = [0, 0];
-        currentScore = 0;
         activePlayer = 0;
+        currentScore = 0;
         playing = true;
 
-        $('#score-0').text('0')
-        $('#score-1').text('0')
-        $('#current-0').text('0')
-        $('#current-1').text('0')
+        score0El.text('0')
+        score1El.text('0')
+        current0El.text('0')
+        current1El.text('0')
 
-        $('.dice').hide()
-        $('.player-0').remove('player-winner')
-        $('.player-1').remove('player-winner')
-        $('.player-0').add('player-active')
-        $('.player-1').remove('player-active')
+        diceEl.hide()
+        player0El.remove('player-winner')
+        player1El.remove('player-winner')
+        player0El.add('player-active')
+        player1El.remove('player-active')
     });
     init();
 
+    //Switch player function
     const switchPlayer = () => {
+        $(`#current-${activePlayer}`).text('0');
+        currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
-        $('.current', activePlayer);
-        textContent = currentScore;
-
+        player0El.replace('player-active');
+        player1El.replace('player-active');
     }
 
     //Rolling dice functionnality
-    $('.btn--roll').click(() => {
+    btnRoll.click(() => {
         if (playing) {
+            //1. Generatin a random dice roll
             const dice = Math.trunc(Math.random() * 6) + 1;
 
-            $('.btn--roll').click(() => {
-                $('.dice').show()
-                $('.dice').attr('src', `./dice-element/dice-${dice}.png`)
-            })
+            diceEl.show()
+            diceEl.attr('src', `./dice-element/dice-${dice}.png`)
 
+            //3. Check for rolled 1: if true, switch to next player
             if (dice !== 1) {
                 currentScore += dice;
-                activePlayer = activePlayer === 0 ? 1 : 0;
-                $('.current', activePlayer) 
-                textContent = currentScore; 
+                $(`#current-${activePlayer}`).text(currentScore)
             } else {
                 switchPlayer()
             }
         }
     });
 
-    //hold value to current score 
-    $('.btn--hold').click(() => {
+    //hold value to current score
+    btnHold.click(() => {
         if (playing) {
-            let score = [0, 0]      
-            score[activePlayer] += currentScore
+            scores[activePlayer] += currentScore ;
 
-            $(`#score-${activePlayer}`).text(score[activePlayer])
+            $(`#score-${activePlayer}`).html(scores[activePlayer]);
 
-            if (score[activePlayer] >= 100) {
+            if (scores[activePlayer] >= 100) {
                 playing = false;
                 $('.dice').hide()
 
-                $(`.player-${activePlayer}`).add('winner-player')
+                $(`.player-${activePlayer}`).add('winner-player');
                 $(`.player-${activePlayer}`).remove('player-active')
             } else {
                 switchPlayer()
             }
         }
-    });
+    })
 
-    $('.btn--new').click(init)
+    btnNew.click(init)
 })
